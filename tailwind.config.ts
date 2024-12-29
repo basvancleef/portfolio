92f38e4a63063fb400dd0bd0f1c6b07407aee955
Bas van Cleef <basvancleef@icloud.com>
2024-12-29 16:32:11 +0100

    chore: Implement TailwindCSS

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type {Config} from 'tailwindcss';
+
+export default {
+    content: [
+        './components/**/*.{js,vue,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+        './app.vue',
+        './error.vue',
+    ],
+    css: ['~/assets/css/tailwind.css'],
+    theme: {
+        extend: {
+            colors: {
+                extra: {
+                    'purple': '#C699FF',
+                    'green': '#1CD760',
+                    'yellow': '#FECE00',
+                    'blue': '#81BCF9',
+                    'gray': '#4D4D4D',
+                    'black': '#161614',
+                }
+            },
+        },
+    },
+} satisfies Config;
